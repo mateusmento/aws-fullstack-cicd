5edb9d59068b983af4426ecab0f3c4008622ae34
Mateus Sarmento <omateusmento@gmail.com>
2024-04-23 15:34:16 -0300

    Test multiple creations of issue

--- api/src/issue/commands/create-issue/create-issue.test.ts
+++ api/src/issue/commands/create-issue/create-issue.test.ts
@@ -3,8 +3,8 @@ import { App, AppBuilder } from "../../../app";
 import createIssueRoute from "./create-issue.route";
 
 const app = new AppBuilder()
-    .withRoutes(createIssueRoute)
-    .build();
+        .withRoutes(createIssueRoute)
+        .build();
 
 beforeEach(() => {
     app.start();
@@ -23,4 +23,14 @@ it('should return created issue in response', async () => {
     expect(result.data).toBeDefined();
     expect(result.data.id).toBe(1);
     expect(result.data.title).toBe("Create issue");
+});
+
+it('should create multiple issues', async () => {
+    let result = await axios.post(BASE_URL + '/issues', { title: 'Create issue' });
+    expect(result.data.id).toBe(1);
+    expect(result.data.title).toBe("Create issue");
+
+    result = await axios.post(BASE_URL + '/issues', { title: 'Remove issue' });
+    expect(result.data.id).toBe(2);
+    expect(result.data.title).toBe("Remove issue");
 });
