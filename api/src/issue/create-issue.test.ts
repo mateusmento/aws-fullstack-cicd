84ae543436e435124e669f56d90ce2d3bc4dd3f4
Mateus Sarmento <omateusmento@gmail.com>
2024-04-23 13:33:29 -0300

    Call endpoint to create issue in failing test

--- api/src/issue/create-issue.test.ts
+++ api/src/issue/create-issue.test.ts
@@ -1,1 +1,8 @@
-it('should create an issue', () => {});
+import axios from "axios";
+
+const BASE_URL = "http://localhost:3000";
+
+it('should create an issue', async () => {
+    const data = { title: 'Create issue' };
+    const result = await axios.post(BASE_URL + '/issues', data);
+});
