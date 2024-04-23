7ce16fd4930c663e8a8fc9dfb5bc215c9bf00670
Mateus Sarmento <omateusmento@gmail.com>
2024-04-23 14:19:24 -0300

    Improve test title

--- api/src/issue/commands/create-issue/create-issue.test.ts
+++ api/src/issue/commands/create-issue/create-issue.test.ts
@@ -13,7 +13,7 @@ afterEach(() => {
 
 const BASE_URL = "http://localhost:3000";
 
-it('should create an issue', async () => {
+it('should return created issue in response', async () => {
     const data = { title: 'Create issue' };
     const result = await axios.post(BASE_URL + '/issues', data);
     expect(result.status).toBe(200);
