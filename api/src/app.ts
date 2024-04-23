31b5f1771aa0d7c5c7c074ec49fe82a08494a3e7
Mateus Sarmento <omateusmento@gmail.com>
2024-04-23 13:56:40 -0300

    Add App class with server initialization and teardown

---
+++ api/src/app.ts
@@ -0,0 +1,18 @@
+import bodyParser from 'body-parser';
+import express from 'express';
+import { Server } from 'http';
+
+export class App {
+    server?: Server;
+
+    start() {
+        const app = express();
+        app.use(bodyParser.json());        
+        app.post('/issues', (req, res) => res.json({}));
+        this.server = app.listen(process.env.PORT ?? 3000);
+    }
+
+    close() {
+        this.server?.close();
+    }
+}
